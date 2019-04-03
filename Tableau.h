1cc19cafa9b6273609306c3eec6a822f78958148
JessyBorcard <48352287+JessyBorcard@users.noreply.github.com>
2019-04-03 12:09:26 +0200

    adding a function
    the function reads a file for storing scores in increasing > decreasing

--- Tableau.h
+++ Tableau.h
@@ -18,10 +18,11 @@
 #include <time.h>
 
 
-void ecritureDocument(char, float);
+void ecritureDocument(char *, float);
+
 int randomiseur();
 
-
+void classement();
 
 
 char tableau_cord[10][10] = {//this is an array where we'll use as a way to show into the command prompt
@@ -39,8 +40,7 @@ char tableau_cord[10][10] = {//this is an array where we'll use as a way to show
 };
 
 
-
-void tableauAffichage (){ //for showing the table as a graphic element
+void tableauAffichage() { //for showing the table as a graphic element
 
 
 
@@ -48,26 +48,26 @@ void tableauAffichage (){ //for showing the table as a graphic element
 
     int ligne = 0, colonne = 0, ligne_aff = 0;
     printf("\n");
-    for (int col_aff = 1; col_aff < 11 ; ++col_aff) {
+    for (int col_aff = 1; col_aff < 11; ++col_aff) {
         printf("    %d ", col_aff); //it shows the upper numbers
     }
     printf("\n");
-    for ( ligne = 0; ligne < 10; ++ligne)
-    {
+    for (ligne = 0; ligne < 10; ++ligne) {
         ligne_aff++;
         for (int i = 0; i < 1; ++i) {
 
             printf("   -----------------------------------------------------------\n");
         }
         for (int loop1 = 0; loop1 < 1; ++loop1) {
-            if (ligne_aff<=9) {
+            if (ligne_aff <= 9) {
                 printf("%d |", ligne_aff); //this is the int to 1 to 9
             }
-            if (ligne_aff>=10){
-                printf("%d|", ligne_aff); //and this one 10 to 99 because it has two numbers in it, the graphic element will shows normally without being offset
+            if (ligne_aff >= 10) {
+                printf("%d|",
+                       ligne_aff); //and this one 10 to 99 because it has two numbers in it, the graphic element will shows normally without being offset
             }
 
-            for ( colonne = 0; colonne < 10; ++colonne) {
+            for (colonne = 0; colonne < 10; ++colonne) {
 
                 printf(" %c   |", tableau_cord[ligne][colonne]); //print every char from the tableau_cord
 
@@ -80,13 +80,10 @@ void tableauAffichage (){ //for showing the table as a graphic element
     printf("   -----------------------------------------------------------\n");
 
 
-
-
-
 }
 
-void tableauChoix(){
-    int col = 11, lig = 11, coup_donne = 0, nbr_de_coup = 0, choix_code, ligne = 0, colonne = 0, num = 0, random = 0;
+void tableauChoix() {
+    int col = 11, lig = 11, coup_donne = 0, nbr_de_coup = 0, choix_code = 0, ligne = 0, colonne = 0, num = 0, random = 0;
     float score = 0; //declaring and initializing variables
     char captain_name[256] = {0};
     int vie_bateau = 0;
@@ -94,36 +91,36 @@ void tableauChoix(){
     int vie_moyen_bateau = 3; //this is how much the boats have squares
     int vie_grand_bateau = 8; //this is how much the boats have squares but this one has two boats, so it takes 4*2
     int vie_immense_beateau = 5; //this is how much the boats have squares
-    static int tableau [10][10] = { //this is where boats are, hard-coded, 1 are small boats, 2 are medium boats, 3 are big boats and 4 the huge boats
-            0,0,1,1,0,0,0,0,0,2,
-            0,0,0,0,0,0,0,0,0,2,
-            0,3,3,3,3,0,0,0,0,2,
-            0,0,0,0,0,0,0,3,0,0,
-            0,0,0,0,0,0,0,3,0,0,
-            0,0,0,0,0,0,0,3,0,0,
-            0,0,0,0,0,0,0,3,0,0,
-            0,0,0,0,0,0,0,0,0,0,
-            0,4,4,4,4,4,0,0,0,0,
-            0,0,0,0,0,0,0,0,0,0,
+    static int tableau[10][10] = { //this is where boats are, hard-coded, 1 are small boats, 2 are medium boats, 3 are big boats and 4 the huge boats
+            0, 0, 1, 1, 0, 0, 0, 0, 0, 2,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 2,
+            0, 3, 3, 3, 3, 0, 0, 0, 0, 2,
+            0, 0, 0, 0, 0, 0, 0, 3, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 3, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 3, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 3, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 4, 4, 4, 4, 4, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 
-    },tableau_choix [10][10] = { 0 };//this is on what the code will work with
+    }, tableau_choix[10][10] = {0};//this is on what the code will work with
 
     random = randomiseur();
 
-    FILE * fptr; //creating a fptr var from FILE
-    if (random == 1){
-    fptr = fopen("tableau1.txt","r");//opening the file Tableaux1.txt
+    FILE *fptr; //creating a fptr var from FILE
+    if (random == 1) {
+        fptr = fopen("tableau1.txt", "r");//opening the file Tableaux1.txt
     } else if (random == 2) {
-        fptr =fopen("tableau2.txt", "r");//opening the file Tableaux2.txt
-    } else if (random == 3){
-        fptr=fopen("tableau3.txt", "r");//opening the file Tableaux3.txt
-    } else
-    {
+        fptr = fopen("tableau2.txt", "r");//opening the file Tableaux2.txt
+    } else if (random == 3) {
+        fptr = fopen("tableau3.txt", "r");//opening the file Tableaux3.txt
+    } else {
         printf("erreur dans le chargement de la carte");
     }
 
 
-    vie_bateau = vie_bateau + vie_petit_bateau + vie_moyen_bateau + vie_grand_bateau + vie_immense_beateau;//adds every life's boat into vie_bateau
+    vie_bateau = vie_bateau + vie_petit_bateau + vie_moyen_bateau + vie_grand_bateau +
+                 vie_immense_beateau;//adds every life's boat into vie_bateau
 
 
 
@@ -136,97 +133,100 @@ void tableauChoix(){
 
 
 
-        printf("Choissez votre carte(1 = carte de base, 2 = carte aléatoire)");
-        scanf("%d", &choix_code);
+    printf("Choissez votre carte(1 = carte de base, 2 = carte aléatoire)");
+    scanf("%d", &choix_code);
 
-        if (choix_code == 1){ // if the choix_code equals 1 the condition becomes true
-            while(ligne < 10) {//do 10 times
-                while(colonne < 10) {//do 10 times
-                    tableau_choix[ligne][colonne] = tableau[ligne][colonne]; //save the array tableau to tableau_choix
-                    colonne++; //adds 1 to a
-                }
-                colonne = 0; //reset colonne
-                ligne++;//adds 1 to i
+    if (choix_code == 1) { // if the choix_code equals 1 the condition becomes true
+        colonne = 0;
+        ligne = 0;
+        while (ligne < 10) {//do 10 times
+            while (colonne < 10) {//do 10 times
+                tableau_choix[ligne][colonne] = tableau[ligne][colonne]; //save the array tableau to tableau_choix
+                colonne++; //adds 1 to a
             }
-        } else if (choix_code == 2){// if the choix_code equals 2 the condition becomes true
-            while(ligne < 10) {//do 10 times
-                while(colonne < 10) {//do 10 times
+            colonne = 0; //reset colonne
+            ligne++;//adds 1 to i
+        }
+    } else if (choix_code == 2) {// if the choix_code equals 2 the condition becomes true
+        colonne = 0;
+        ligne = 0;
+        while (ligne < 10) {//do 10 times
+            while (colonne < 10) {//do 10 times
 
-                    if (fptr == NULL){
-                        printf("Error! opening file");
+                if (fptr == NULL) {
+                    printf("Error! opening file");
 
-                        // Program exits if the file pointer returns NULL.
-                        exit(1);
-                    }
-                    num = fgetc(fptr); // reading fptr which reads Tableaux.txt
-
-
-
-
-                    if(num == 48){
-                        num = 0;
-                    }
-                    if (num == 49){
-                        num = PETIT_BATEAU; //convert ascii to binary
-                    }
-                    if (num == 50){
-                        num = BATEAU_MOYEN;//convert ascii to binary
-                    }
-
-                    if(num == 51){
-                        num = GRAND_BATEAU;//convert ascii to binary
-                    }
-                    if(num == 52){
-                        num = IMMENSE_BATEAU;//convert ascii to binary
-                    }
-                    tableau_choix[ligne][colonne] = num; //save num into tableau_choix
-
-                    colonne++; //adds 1 to a
-
+                    // Program exits if the file pointer returns NULL.
+                    exit(1);
                 }
-                colonne = 0; //reset colonne
-                ligne++;//adds 1 to i
+                num = fgetc(fptr); // reading fptr which reads Tableaux.txt
+
+
+
+
+                if (num == 48) {
+                    num = 0;
+                }
+                if (num == 49) {
+                    num = PETIT_BATEAU; //convert ascii to binary
+                }
+                if (num == 50) {
+                    num = BATEAU_MOYEN;//convert ascii to binary
+                }
+
+                if (num == 51) {
+                    num = GRAND_BATEAU;//convert ascii to binary
+                }
+                if (num == 52) {
+                    num = IMMENSE_BATEAU;//convert ascii to binary
+                }
+                tableau_choix[ligne][colonne] = num; //save num into tableau_choix
+
+                colonne++; //adds 1 to a
+
             }
-            fclose(fptr); // close tableaux.txt
+            colonne = 0; //reset colonne
+            ligne++;//adds 1 to i
+        }
+        fclose(fptr); // close tableaux.txt
+    }
 
 
     printf("en combien de coup voulez vous jouer ? ");
     scanf("%d", &nbr_de_coup); //save the input to nbre_de_coup
 
-    do
-    {
+    do {
         printf("\t\t-----la carte de %s-----", captain_name);
-
 
 
         tableauAffichage();
 
 
-
-        do {
+        do { //this loop will be true if the input of lig or col are smaller or bigger than 1 or 10
 
 
             printf("\nchoix de Ligne : ");
             scanf("%d", &lig); //save the input to lig
-            lig--;
+            lig--;//convert for entering the array more easily
 
             printf("\nchoix de Colonne : ");
             scanf("%d", &col); //save the input to col
-            col--;
+            col--;//convert for entering the array more easily
 
-            if(lig > 10 || col > 10){
+            if ((lig > 10 || col > 10) &&
+                (lig < 1 || col < 1)) { //test if lig or col is smaller or bigger than 1 or 10
                 printf("des valeurs entre 1 et 10!!\n");
 
             }
 
-        } while (lig > 10 || col > 10);
-        system("cls");
+        } while (lig < 0 || col < 0 || lig > 11 || col > 11);
+        system("cls"); //clear, for a better view
 
-        if (tableau_cord[lig][col] == *"X" || tableau_cord[lig][col] == *"-"){
+        if (tableau_cord[lig][col] == *"X" || tableau_cord[lig][col] == *"-") { //test if the case is already chosen
             printf("cette case est déjà touchée\n");
         }
 
-        if(tableau_choix[lig][col] == 0){
+        if (tableau_choix[lig][col] == 0) {
             printf("loupé! \n\n\n"); //if the player's choice of lig and col equals 0 it says loupé
             tableau_cord[lig][col] = *"-";
         }
@@ -234,8 +234,9 @@ void tableauChoix(){
 
         if (tableau_cord[lig][col] == *"X") {//beep
 
-        }else {
-            if (tableau_choix[lig][col] == PETIT_BATEAU) { // if the player's choice of lig and col equals 1 the result is true
+        } else {
+            if (tableau_choix[lig][col] ==
+                PETIT_BATEAU) { // if the player's choice of lig and col equals 1 the result is true
                 tableau_cord[lig][col] = *"X"; //replacing the tableau_cord value depending on lig and col by an X
                 printf("\ntouché! \n\n\n");
                 score++; //adds 1 to score
@@ -251,7 +252,8 @@ void tableauChoix(){
 
 
             }
-            if (tableau_choix[lig][col] == BATEAU_MOYEN) { // if the player's choice of lig and col equals 2 the result is true
+            if (tableau_choix[lig][col] ==
+                BATEAU_MOYEN) { // if the player's choice of lig and col equals 2 the result is true
                 tableau_cord[lig][col] = *"X"; //replacing the tableau_cord value depending on lig and col by an X
                 printf("\ntouché! \n\n\n");
                 score++; //adds 1 to score
@@ -264,7 +266,8 @@ void tableauChoix(){
                     score = BATEAU_MOYEN * SCORE_MULTIPLIER;
                 }
             }
-            if (tableau_choix[lig][col] == GRAND_BATEAU) { // if the player's choice of lig and col equals 3 the result is true
+            if (tableau_choix[lig][col] ==
+                GRAND_BATEAU) { // if the player's choice of lig and col equals 3 the result is true
                 tableau_cord[lig][col] = *"X"; //replacing the tableau_cord value depending on lig and col by an X
                 printf("\ntouché! \n\n\n");
                 score++; //adds 1 to score
@@ -277,7 +280,8 @@ void tableauChoix(){
                     score = GRAND_BATEAU * SCORE_MULTIPLIER;
                 }
             }
-            if (tableau_choix[lig][col] == IMMENSE_BATEAU) { // if the player's choice of lig and col equals 4 the result is true
+            if (tableau_choix[lig][col] ==
+                IMMENSE_BATEAU) { // if the player's choice of lig and col equals 4 the result is true
                 tableau_cord[lig][col] = *"X"; //replacing the tableau_cord value depending on lig and col by an X
                 printf("\ntouché! \n\n\n");
                 score++; //adds 1 to score
@@ -294,44 +298,34 @@ void tableauChoix(){
                 printf("vous avez gagner %s !\n", captain_name);
                 printf(" nombres de coup : %d \n", coup_donne); // if vie de bateau equals 0 the victory menu shows
                 score = score / coup_donne;
-                ecritureDocument(captain_name, score); //calls ecriture document with parameters captain_name and score
+                ecritureDocument(captain_name,
+                                 score); //calls ecriture document with parameters captain_name and score
                 system("pause");
 
             }
         }
 
 
-
-
-
-
-
-
-
         coup_donne++;
     } while (coup_donne < nbr_de_coup); //this loop makes loops depending on how many times the player wants to play
-    }
 }
-int menu(){ // this is the main menu
+
+
+int menu() { // this is the main menu
 
 
     char choix = 0;
 
 
-
-
     printf("--------Bataille Navale! 0.1--------\n");
-
-
-
-
 
 
     do {
         printf("1.Jouer \n");
         printf("2.Aide de jeu\n");
         printf("3.credits\n");
-        printf("4.quiitez le programme\n");
+        printf("4.Classement\n");
+        printf("4.Quittez le programme\n");
         printf("votre choix :");
         scanf("%s", &choix);
         switch (choix) {
@@ -346,42 +340,100 @@ int menu(){ // this is the main menu
                 printf("\nchoisissez une case et notez la en bas du programme, si la case n'est pas valable, le programme vous redemendra. \n");
                 break;
             case '3':
-                    printf("ce projet a été créé par Jessy borcard.");
+                printf("ce projet a été créé par Jessy borcard.");
+
                 break;
             case '4':
+                classement();
+                break;
+            case '5':
                 return 1;
         }
-    }while(choix > *"4"); //loop for re-asking the question if choix is bigger than 3
+    } while (choix > *"4"); //loop for re-asking the question if choix is bigger than 3
 
 
 }
 
 
+void ecritureDocument(char *nom_captaine,
+                      float score) { //this function has two parameters, it write down char and int into a file called score
 
-void ecritureDocument(char nom_captaine, float score){ //this function has two parameters, it write down char and int into a file called score
-
-    FILE*fp;
-
+    FILE *fp;
 
 
     fp = fopen("Score.txt", "w"); //open Score.txt
     fputs(nom_captaine, fp); //put the name of the captain into fp
     fputs("-", fp); //put a - into fp
     fprintf(fp, "%0.3f", score); //print into fp score as int
+    fputs("-", fp);
+    fputs("\n", fp); //add a backspace in the file
     fclose(fp); //close fp
 }
 
 
-int randomiseur()
-{
-    int valeur=0;
+int randomiseur() {
+    int valeur = 0;
 
     srand((unsigned) time(NULL)); //numbers will always be different
-    valeur= 1 + rand() % 2; //numbers between 1 and 2
+    valeur = rand() % 2; //numbers between 0 and 2
+    valeur = valeur + 1; //add one
 
     return valeur;
 
 };
+
+
+void classement() {
+
+    FILE*fp = 0; //create a var that can handle file opening
+    float score = 0;
+    int   a =0; //init vars and array
+    float score_tableau [100] = {0};
+
+
+
+    fp = fopen("Score.txt", "r"); //opening a file
+    while(!feof(fp)) { //do this loop until it reaches the end of file
+
+
+        fscanf(fp, "%f", &score); //Scanning float type in the file
+
+        score_tableau[a] = score; //storing them in a array
+
+        a++;
+
+    }
+    for (int loop1 = 0; loop1 < 100; loop1++)                     //Loop for ascending ordering
+    {
+        for (int loop2 = 0; loop2 < 100; loop2++)             //Loop for comparing other values
+        {
+            if (score_tableau[loop2] < score_tableau[loop1])                //Comparing other array elements
+            {
+                float tmp = score_tableau[loop1];         //Using temporary variable for storing last value
+                score_tableau[loop1] = score_tableau[loop2];            //replacing value
+                score_tableau[loop2] = tmp;             //storing last value
+            }
+        }
+    }
+a = 1;
+    for (int loop1  = 0; loop1  < 100; loop1 ++)                     //Loop for printing array data after sorting
+    {
+        if(score_tableau[loop1] != 0){
+
+
+            printf("\n%d %0.3f",a, score_tableau[loop1]); //printing the table plus a
+            a++;
+        }
+    }
+
+
+
+    fclose(fp); //close fp
+
+
+
+    //TODO make a function that create a score table
+}
 
 
 #endif //BATAILLENAVALE_TABLEAU_H
